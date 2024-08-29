074daff4403b6f5024fb6ed61788e89a43e279af
BrianGonzzza <braiang177@gmail.com>
2024-08-28 22:29:07 -0300

    eventos

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.querySelector('div');
+
+    div.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
